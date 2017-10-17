4ea41be6667c9c6eed1d57a62e9892eddc71923a
Ryan LaBouve <imryanlabouve@gmail.com>
2017-10-17 01:25:03 -0500

    Adds binary file

---
+++ bin/git-told.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+let gitTold = require('../index.js');
+gitTold();
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { log } = console;
 const say = require('./lib/say.js');
 
-let head = path.join(__dirname, '.git', 'HEAD');
+let head = path.join(process.cwd(), '.git', 'HEAD');
 
 let readFile = (filename, cb) => {
   fs.readFile(filename, 'utf-8', (err, data) => {
@@ -16,14 +16,19 @@ let readFile = (filename, cb) => {
 
 let branchNameFromHEAD = (ref) => ref.split('/').slice(-1)[0];
 
-gaze(head, function(err, watcher) {
-  this.on('changed', function(filepath) {
-    readFile(head, (data) => {
-      let content = branchNameFromHEAD(data);
-      let operation = 'branch';
-      say({content, operation});
+let init = (params = {}) => {
+  // Kick off the watcher
+  gaze(head, function(err, watcher) {
+    this.on('changed', function(filepath) {
+      readFile(head, (data) => {
+        let content = branchNameFromHEAD(data);
+        let operation = 'branch';
+        say({content, operation});
+      });
     });
   });
-});
 
-log(`Starting to watch ${head} for changes`);
+  log(`Starting to watch ${head} for changes`);
+}
+
+module.exports = init;
