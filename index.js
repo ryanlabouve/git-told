4364fb5511624b3c149c479e4e38a30016772303
Ryan LaBouve <imryanlabouve@gmail.com>
2017-10-17 00:31:57 -0500

    Working version

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 'use strict'
+const fs = require('fs');
 const gaze = require('gaze');
 const path = require('path');
 const { log } = console;
 
 let head = path.join(__dirname, '.git', 'HEAD');
 
+let readFile = (filename, cb) => {
+  fs.readFile(filename, 'utf-8', (err, data) => {
+    if (err) throw err;
+    cb(data);
+  });
+}
+
+let branchNameFromHEAD = (ref) => ref.split('/').slice(-1)[0];
+
 gaze(head, function(err, watcher) {
   this.on('changed', function(filepath) {
-    console.log(filepath + ' was changed');
+    readFile(head, (data) => {
+      let sayContent = branchNameFromHEAD(data);
+      console.log(sayContent);
+    });
   });
 });
 
